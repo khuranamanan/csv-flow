d40f41a2c2266656df08fcb232e584ba5d036136
Manan Khurana <khuranamanan12@gmail.com>
2025-01-19 15:46:43 +0530

    Add theme switching functionality and UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeSwitch } from "./components/theme-switch";
+
 function App() {
-  return <>App</>;
+  return (
+    <div className="flex justify-center items-center min-h-dvh relative">
+      App
+      <div className="absolute bottom-4 left-4">
+        <ThemeSwitch />
+      </div>
+    </div>
+  );
 }
 
 export default App;
